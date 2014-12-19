94fa3a07c472707ee5746cac3bb0aa986f4c3deb
alexandrpunk <alexandrpunk@gmail.com>
2014-12-19 02:02:11 -0700

    terminado el diseño de editar cvu

--- app/views/cvu/editar.blade.php
+++ app/views/cvu/editar.blade.php
@@ -2,77 +2,100 @@
 @section('content')
 <div class="row">
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-user" aria-hidden="true"></i>
-          <p><a href="#" >Informacion personal</a></p>
+          <img src="/img/icons/man.png">
+          <p>Informacion personal</p>
       </div> 
+    </a>
  </div>
 <div class="col-md-3">
     <a href="#">
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-google_maps" aria-hidden="true"></i>
+          <img src="/img/icons/address.png">
           <p>Direcciones</p>
-      </div> </a>
+      </div>
+    </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-book_open" aria-hidden="true"></i>
-          <p><a href="#" >Escuelas</a></p>
+          <img src="/img/icons/school.png">
+          <p>Escuelas</p>
       </div> 
+    </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-education" aria-hidden="true"></i>
-          <p><a href="#" >Titulos</a></p>
-      </div> 
+          <img src="/img/icons/title.png">
+          <p>Titulos</p>
+      </div>
+    </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-certificate" aria-hidden="true"></i>
-          <p><a href="#" >Especialidades</a></p>
-      </div> 
+          <img src="/img/icons/especial.png">
+          <p>Especialidades</p>
+      </div>
+    </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-certificate" aria-hidden="true"></i>
-          <p><a href="#" >Especialidades</a></p>
-      </div> 
+          <img src="/img/icons/curso.png">
+          <p>Cursos</p>
+      </div>
+    </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-certificate" aria-hidden="true"></i>
-          <p><a href="#" >Especialidades</a></p>
-      </div> 
+          <img src="/img/icons/publish.png">
+          <p>Publicaciones</p>
+      </div>
+    </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-certificate" aria-hidden="true"></i>
-          <p><a href="#" >Especialidades</a></p>
-      </div> 
+          <img src="/img/icons/tech.png">
+          <p>Tecnologias</p>
+      </div>
+    </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-certificate" aria-hidden="true"></i>
-          <p><a href="#" >Especialidades</a></p>
-      </div> 
+          <img src="/img/icons/idiom.png">
+          <p>Idiomas</p>
+      </div>
+    </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-certificate" aria-hidden="true"></i>
-          <p><a href="#" >Especialidades</a></p>
-      </div> 
+          <img src="/img/icons/hist.png">
+          <p>Proyectos</p>
+      </div>
+        </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-certificate" aria-hidden="true"></i>
-          <p><a href="#" >Especialidades</a></p>
-      </div> 
+          <img src="/img/icons/bussines.png">
+          <p>Trabajos</p>
+      </div>
+        </a>
  </div>
 <div class="col-md-3">
+    <a>
       <div class="option-block">
-          <i class="cvu-opt glyphicon glyphicon-certificate" aria-hidden="true"></i>
-          <p><a href="#" >Especialidades</a></p>
+          <img src="/img/icons/files.png">
+          <p>Archivos</p>
       </div> 
+    </a>
  </div>
 </div>
 @stop
--- app/views/includes/lateral.blade.php
+++ app/views/includes/lateral.blade.php
@@ -4,15 +4,15 @@
             <p class="text-center small">{{ HTML::link('logout', 'Cerrar Sesion') }}</p>
               <ul class="nav nav-list">
                                       <li class="nav-divider"></li>
-                <li class="nav-header"><i class="small glyphicon glyphicon-vcard" aria-hidden="true"></i> Informacion personal</li>
+                <li class="nav-header"><i class="small glyphicon glyphicon-vcard" aria-hidden="true"></i> Perfil de usuario</li>
                 <li>
                     <a href="#">
-                        <i class="small glyphicon glyphicon-eye_open" aria-hidden="true"></i>&nbsp;&nbsp;Ver Informacion Personal
+                        <i class="small glyphicon glyphicon-eye_open" aria-hidden="true"></i>&nbsp;&nbsp;Ver Perfil de Usuario
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <i class="small glyphicon glyphicon-edit" aria-hidden="true"></i>&nbsp;&nbsp;Editar Informacion Personal
+                        <i class="small glyphicon glyphicon-edit" aria-hidden="true"></i>&nbsp;&nbsp;Editar Perfil de Usuario
                     </a>
                 </li>
                 <li class="nav-divider"></li>
